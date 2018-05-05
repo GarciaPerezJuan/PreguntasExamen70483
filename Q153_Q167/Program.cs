cffb21e12ac3a04fd08ea7782f1605860e676315
GarciaPerezJuan <juan.garcia.perez.24@gmail.com>
2018-05-05 18:20:59 +0200

    Implemento Q153 y añado Q217

--- Q153_Q167/Program.cs
+++ Q153_Q167/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Q153_Q167
@@ -10,6 +11,44 @@ namespace Q153_Q167
     {
         static void Main(string[] args)
         {
+            TestIfWebSite("http://www.as.com");
+        }
+
+        public static List<string> TestIfWebSite(string url)
+        {
+            const string pattern = @"http://(www\.)?([^\.]+)\.com";
+            List<string> result = new List<string>();
+
+            MatchCollection myMatches = Regex.Matches(url, pattern);
+
+            // Q153. A. Incorrecta. Da error de ejecución. typeMismatch
+            //result = (List<string>) myMatches.GetEnumerator();
+
+            // Q153. B. Incorrecta. SyncRoot no devuelve los elementos que corresponden con el pattern
+            //result = (List<string>)myMatches.SyncRoot;
+
+            // Q153. C. Correcta.
+            result = (from Match m in myMatches
+                      select m.Value).ToList<string>();
+
+            // Q153. D. Incorrecta. Devuelve justo las que no corresponden con el pattern
+            //result = (from Match m in myMatches where !m.Success select m.Value).ToList<string>();
+
+            // Q167. A. Incorrecta. No devuelve el valor.
+            foreach (Match c in myMatches)
+                result.Add(c.Groups.ToString());
+
+            // Q167. B. Incorrecta.Da error de ejecución. typeMismatch
+            //result = (List<string>) myMatches.GetEnumerator();
+
+            // Q167. C. Correcta
+            foreach (Match c in myMatches)
+                result.Add(c.Value);
+
+            // Q167. D. Incorrecta. SyncRoot no devuelve los elementos que corresponden con el pattern
+            //result = (List<string>)myMatches.SyncRoot;
+            
+            return result;
         }
     }
 }
---
+++ Q217/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myData = new List<string>();
+            myData.Add("string1");
+            myData.Add("string2");
+            myData.Add("string3");
+
+            // El objetivo de la pregunta es borrar el contenido de la lista. Que trozo de código hay que elegit?
+
+            // A. Incorrecta. No borra todas ya que el count va bajando y la i subiendo. Deja una sin borrar.
+            //for (int i = 0; i <= myData.Count; i++)
+            //{
+            //    myData.RemoveAt(i);
+            //}
+
+            // B. Correcta.
+            while (myData.Count != 0)
+            {
+                myData.RemoveAt(0);
+            }
+
+            // C. Incorrecta. Al eliminar un elemento considera la colección modificada. Error en tiempo de ejecución.
+            //foreach (string c in myData)
+            //{
+            //    myData.Remove(c);
+            //}
+
+            //D. Incorrecta. No borra todas ya que el count va bajando y la i subiendo. Deja una sin borrar.
+            //for (int i = 0; i <= myData.Count; i++)
+            //{
+            //    myData.RemoveAt(0);
+            //}
+
+        }
+    }
+}
